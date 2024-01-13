d17b3561e40add6c4a739a97b8844fb6f8f0f16c
yindaheng98 <yindaheng98@gmail.com>
2024-01-13 15:46:00 -0800

    export2npz

--- scripts/parse_seq.py
+++ scripts/parse_seq.py
@@ -1,11 +1,13 @@
 import argparse
 import bson
 import numpy as np
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--start", type=int, required=True, help="The start frame number.")
 parser.add_argument("--end", type=int, required=True, help="The end frame number.")
 parser.add_argument("--saveformat", type=str, required=True, help="The path format of the saved snapshot.")
+parser.add_argument("--exportformat", type=str, required=True, help="The path format of exported video file (.npz).")
 
 def load_save(path):
     with open(path, "rb") as f:
@@ -16,7 +18,7 @@ def load_params(save):
     params_bin = snapshot['params_binary']
     params_type = snapshot['params_type']
     if params_type == "__half":
-        return np.frombuffer(params_bin, dtype=np.float16)
+        return np.frombuffer(params_bin, dtype=np.float16).astype(np.float32)
     else:
         return np.frombuffer(params_bin, dtype=np.float32)
 
@@ -24,13 +26,14 @@ if __name__ == "__main__":
     import os
     args = parser.parse_args()
     root = os.getcwd()
+    os.makedirs(os.path.dirname(args.exportformat), exist_ok=True)
     savepath = os.path.join(root, args.saveformat % args.start)
-    os.makedirs(os.path.dirname(savepath), exist_ok=True)
     save = load_save(savepath)
     params = load_params(save)
-    for i in range(args.start + 1, args.end + 1):
+    np.savez_compressed(args.exportformat % args.start, arr_0=params)
+    for i in tqdm(range(args.start + 1, args.end + 1)):
         savepath = os.path.join(root, args.saveformat % i)
         save = load_save(savepath)
         last_params, params = params, load_params(save)
-        params_diff = params - last_params
-        print(last_params, params)
+        np.savez_compressed(args.exportformat % i, arr_0=params)
+
