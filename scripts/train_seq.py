56a90ad7234f152f31cf7c8ad4e4aab22de1366d
yindaheng98 <yindaheng98@gmail.com>
2024-01-12 22:14:18 -0800

    train a video

---
+++ scripts/train_seq.py
@@ -0,0 +1,29 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--start", type=int, required=True, help="The start frame number.")
+parser.add_argument("--end", type=int, required=True, help="The end frame number.")
+parser.add_argument("--dataformat", type=str, required=True, help="The path format of the frames data.")
+parser.add_argument("--saveformat", type=str, required=True, help="The path format of the snapshot to save.")
+parser.add_argument("--executable", type=str, required=True, help="The path to the trainer executable.")
+parser.add_argument("--steps", type=int, required=True, help="How many steps do you want to train.")
+
+if __name__ == "__main__":
+    import os
+    import subprocess
+    args = parser.parse_args()
+    root = os.getcwd()
+    datapath = os.path.join(root, args.dataformat % args.start)
+    savepath = os.path.join(root, args.saveformat % args.start)
+    executable = os.path.join(root, args.executable)
+    os.makedirs(os.path.dirname(savepath), exist_ok=True)
+    cmd = [executable, f"--step={args.steps}", f"--save_snapshot={savepath}", datapath]
+    print(cmd)
+    subprocess.run(cmd)
+    for i in range(args.start + 1, args.end + 1):
+        last_savepath = savepath
+        datapath = os.path.join(root, args.dataformat % i)
+        savepath = os.path.join(root, args.saveformat % i)
+        cmd = [executable, f"--step={args.steps}", f"--save_snapshot={savepath}", f"--snapshot={last_savepath}", "--freeze", datapath]
+        print(cmd)
+        subprocess.run(cmd)
