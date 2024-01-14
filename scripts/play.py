36690e6faea6df5f1fd987db79ae5c77a3542532
yindaheng98 <yindaheng98@gmail.com>
2024-01-13 16:03:02 -0800

    a player, from run.py

---
+++ scripts/play.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2020-2022, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+import argparse
+import os
+import commentjson as json
+
+import numpy as np
+
+import shutil
+import time
+
+from common import *
+from scenes import *
+
+from tqdm import tqdm
+
+import pyngp as ngp # noqa
+
+def parse_args():
+	parser = argparse.ArgumentParser(description="Run instant neural graphics primitives with additional configuration & output options")
+
+
+	parser.add_argument("--network", default="", help="Path to the network config. Uses the scene's default if unspecified.")
+
+	parser.add_argument("--load_snapshot", "--snapshot", default="", help="Load this snapshot before training. recommended extension: .ingp/.bson")
+
+	parser.add_argument("--nerf_compatibility", action="store_true", help="Matches parameters with original NeRF. Can cause slowness and worse results on some scenes, but helps with high PSNR on synthetic scenes.")
+	parser.add_argument("--exposure", default=0.0, type=float, help="Controls the brightness of the image. Positive numbers increase brightness, negative numbers decrease it.")
+
+	parser.add_argument("--width", "--screenshot_w", type=int, default=0, help="Resolution width of GUI and screenshots.")
+	parser.add_argument("--height", "--screenshot_h", type=int, default=0, help="Resolution height of GUI and screenshots.")
+
+	parser.add_argument("--n_steps", type=int, default=-1, help="Number of steps to train for before quitting.")
+	parser.add_argument("--second_window", action="store_true", help="Open a second window containing a copy of the main output.")
+	parser.add_argument("--vr", action="store_true", help="Render to a VR headset.")
+
+	parser.add_argument("--sharpen", default=0, help="Set amount of sharpening applied to NeRF training images. Range 0.0 to 1.0.")
+
+
+	return parser.parse_args()
+
+def get_scene(scene):
+	for scenes in [scenes_sdf, scenes_nerf, scenes_image, scenes_volume]:
+		if scene in scenes:
+			return scenes[scene]
+	return None
+
+if __name__ == "__main__":
+	args = parse_args()
+
+	testbed = ngp.Testbed()
+	testbed.root_dir = ROOT_DIR
+
+	sw = args.width or 1920
+	sh = args.height or 1080
+	while sw * sh > 1920 * 1080 * 4:
+		sw = int(sw / 2)
+		sh = int(sh / 2)
+	testbed.init_window(sw, sh, second_window=args.second_window)
+	if args.vr:
+		testbed.init_vr()
+
+
+	if args.load_snapshot:
+		scene_info = get_scene(args.load_snapshot)
+		if scene_info is not None:
+			args.load_snapshot = default_snapshot_filename(scene_info)
+		testbed.load_snapshot(args.load_snapshot)
+	elif args.network:
+		testbed.reload_network_from_file(args.network)
+
+	if testbed.mode == ngp.TestbedMode.Sdf:
+		testbed.tonemap_curve = ngp.TonemapCurve.ACES
+
+	testbed.nerf.sharpen = float(args.sharpen)
+	testbed.exposure = args.exposure
+	testbed.shall_train = False
+
+
+	testbed.nerf.render_with_lens_distortion = True
+
+	network_stem = os.path.splitext(os.path.basename(args.network))[0] if args.network else "base"
+	if testbed.mode == ngp.TestbedMode.Sdf:
+		setup_colored_sdf(testbed, args.scene)
+
+	if args.nerf_compatibility:
+		print(f"NeRF compatibility mode enabled")
+
+		# Prior nerf papers accumulate/blend in the sRGB
+		# color space. This messes not only with background
+		# alpha, but also with DOF effects and the likes.
+		# We support this behavior, but we only enable it
+		# for the case of synthetic nerf data where we need
+		# to compare PSNR numbers to results of prior work.
+		testbed.color_space = ngp.ColorSpace.SRGB
+
+		# No exponential cone tracing. Slightly increases
+		# quality at the cost of speed. This is done by
+		# default on scenes with AABB 1 (like the synthetic
+		# ones), but not on larger scenes. So force the
+		# setting here.
+		testbed.nerf.cone_angle_constant = 0
+
+		# Match nerf paper behaviour and train on a fixed bg.
+		testbed.nerf.training.random_bg_color = False
+
+	old_training_step = 0
+	n_steps = args.n_steps
+
+	tqdm_last_update = 0
+	while testbed.frame():
+		if testbed.want_repl():
+			repl(testbed)
+		testbed.reset_accumulation()
+
