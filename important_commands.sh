1659a9f3ee9ad2f68471e4a0ece38c246be85050
yindaheng98 <yindaheng98@gmail.com>
2024-01-13 17:06:55 -0800

    train it

--- important_commands.sh
+++ important_commands.sh
@@ -1,2 +1,2 @@
 python3 scripts/stnerf2nerf.py --aabb_scale 8 --path data/nerf/walking
-python3 scripts/train_seq.py --start 1 --end 75 --dataformat "data/nerf/walking/frame%d" --saveformat "results/stnerf-walking/frame%d.bson" --executable "./instant-ngp-train" --steps 1000
+python3 scripts/train_seq.py --steps 10000 --start 1 --end 75 --dataformat "data/nerf/walking/frame%d" --saveformat "results/stnerf-walking/frame%d.bson" --executable "./instant-ngp-train"
