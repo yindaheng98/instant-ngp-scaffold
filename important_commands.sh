ddc46a7c6d5ebb454172d132ad90873f4d25d42c
yindaheng98 <yindaheng98@gmail.com>
2024-01-14 21:31:28 -0800

    train taekwondo

--- important_commands.sh
+++ important_commands.sh
@@ -1,2 +1,3 @@
 python3 scripts/stnerf2nerf.py --aabb_scale 8 --path data/nerf/walking
 python3 scripts/train_seq.py --steps 10000 --start 1 --end 75 --dataformat "data/nerf/walking/frame%d" --saveformat "results/stnerf-walking/frame%d.bson" --executable "./instant-ngp-train"
+python3 scripts/train_seq.py --steps 10000 --start 1 --end 101 --dataformat "data/nerf/taekwondo/frame%d" --saveformat "results/stnerf-taekwondo/frame%d.bson" --executable "./instant-ngp-train"
