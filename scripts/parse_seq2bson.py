27256444144177faa73c84e6fde2559de043ae90
yindaheng98 <yindaheng98@gmail.com>
2024-01-17 22:40:35 -0800

    diff bug is large

--- scripts/parse_seq2bson.py
+++ scripts/parse_seq2bson.py
@@ -15,7 +15,7 @@ parser.add_argument("--interdiffexportformat", type=str, required=True, help="Th
 def load_save(path):
     with open(path, "rb") as f:
         return bson.decode_all(f.read())[0]
-    
+
 def load_params(save):
     snapshot = save['snapshot']
     params_bin = snapshot['params_binary']
@@ -29,9 +29,31 @@ def load_params(save):
         raise ValueError("params has NaN!")
     if np.isnan(density_grid).any():
         raise ValueError("density_grid has NaN!")
+
+    params, density_grid = np.copy(params), np.copy(density_grid)
+
+    if np.isposinf(params).any():
+        replace = params[~np.isposinf(params)].max()
+        print("+inf in params, replace by", replace)
+        params[np.isposinf(params)] = replace
+    if np.isneginf(params).any():
+        replace = params[~np.isneginf(params)].min()
+        print("-inf in params, replace by", replace)
+        params[np.isneginf(params)] = replace
+
+    if np.isposinf(density_grid).any():
+        replace = density_grid[~np.isposinf(density_grid)].max()
+        print("+inf in density_grid, replace by", replace)
+        density_grid[np.isposinf(density_grid)] = replace
+    if np.isneginf(density_grid).any():
+        replace = density_grid[~np.isneginf(density_grid)].max()
+        print("-inf in density_grid, replace by", replace)
+        density_grid[np.isneginf(density_grid)] = replace
+
     return params, density_grid
 
 T = 1e-6
+T_TOOBIG = 60000
 
 if __name__ == "__main__":
     import os
@@ -51,7 +73,8 @@ if __name__ == "__main__":
         }))
     last_diff_params, last_diff_density_grid = np.copy(params), np.copy(density_grid).astype(np.float32)
     last_intr_params, last_intr_density_grid = np.copy(params), np.copy(density_grid)
-    for i in tqdm(range(args.start + 1, args.end + 1)):
+    for i in range(args.start + 1, args.end + 1):
+        print("do", i-args.start, "/", args.end-args.start)
         savepath = os.path.join(root, args.saveformat % i)
         save = load_save(savepath)
         params, density_grid = load_params(save)
@@ -63,10 +86,18 @@ if __name__ == "__main__":
                 "density_grid": density_grid.tobytes(),
             }))
 
+        absolute_diff_density_grid = density_grid.astype(np.float32) - last_diff_density_grid
+        relative_diff_density_grid = absolute_diff_density_grid / last_diff_density_grid
+        relative_diff_density_grid[np.isnan(relative_diff_density_grid)] = absolute_diff_density_grid[np.isnan(relative_diff_density_grid)]
+        big_diff_density_grid_idx = (np.abs(relative_diff_density_grid) > T) & (np.abs(absolute_diff_density_grid) > T)
+        toobig_diff_density_grid_idx = absolute_diff_density_grid > T_TOOBIG
+
         diff_params = params - last_diff_params
         diff_density_grid = density_grid.astype(np.float32) - last_diff_density_grid
         diff_params[np.abs(diff_params) <= T] = 0
-        diff_density_grid[np.abs(diff_density_grid) <= T] = 0
+        diff_density_grid[~big_diff_density_grid_idx] = 0
+        diff_density_grid[diff_density_grid > T_TOOBIG] = T_TOOBIG
+        diff_density_grid[diff_density_grid < -T_TOOBIG] = -T_TOOBIG
         with open(args.interdiffexportformat % i, "wb") as f:
             f.write(bson.encode({
                 "params_size": diff_params.shape[0],
@@ -76,6 +107,8 @@ if __name__ == "__main__":
             }))
         last_diff_params += diff_params
         last_diff_density_grid += diff_density_grid
+        print("error of params", (params - last_diff_params).max(), (params - last_diff_params).min())
+        print("error of density_grid", (density_grid - diff_density_grid).max(), (density_grid - diff_density_grid).min())
 
         intr_params_idx = np.where(np.abs(params - last_intr_params) > T)[0].astype(np.uint32)
         intr_density_grid_idx = np.where(np.abs(density_grid - last_intr_density_grid) > T)[0].astype(np.uint32)
