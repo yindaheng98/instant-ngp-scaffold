91a474c0e665afc01f6ed874fd8ab22fdee3b77b
yindaheng98 <yindaheng98@gmail.com>
2024-01-20 12:51:48 -0800

    save size

--- scripts/parse_seq2csr.py
+++ scripts/parse_seq2csr.py
@@ -1,8 +1,9 @@
 import argparse
-import bson
+import json
 import numpy as np
 import zlib
 from scipy import sparse
+from parse_seq2bson import load_save, load_params, compute_diff_params, compute_diff_density_grid
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--start", type=int, required=True, help="The start frame number.")
@@ -12,47 +13,21 @@ parser.add_argument("--intraexportformat", type=str, required=True, help="The pa
 parser.add_argument("--interexportformat", type=str, required=True, help="The path format of exported inter-frame video frames (.bson).")
 parser.add_argument("-T", type=float, required=True, help="Threshold for set zero in inter frames.")
 
-def load_save(path):
-    with open(path, "rb") as f:
-        return bson.decode_all(f.read())[0]
+def get_size(data):
+    data_compressed = zlib.compress(data.tobytes())
+    data_csr = sparse.csr_matrix(data)
+    data_csr_compressed = zlib.compress(data_csr.data.tobytes())
+    return len(data_compressed), len(data_csr_compressed)
 
-def load_params(save):
-    snapshot = save['snapshot']
-    params_bin = snapshot['params_binary']
-    params_type = snapshot['params_type']
-    density_grid_bin = snapshot['density_grid_binary']
-    if params_type == "__half":
-        params, density_grid = np.frombuffer(params_bin, dtype=np.float16), np.frombuffer(density_grid_bin, dtype=np.float16)
-    else:
-        params, density_grid = np.frombuffer(params_bin, dtype=np.float32).astype(np.float16), np.frombuffer(density_grid_bin, dtype=np.float16)
-    if np.isnan(params).any():
-        raise ValueError("params has NaN!")
-    if np.isnan(density_grid).any():
-        raise ValueError("density_grid has NaN!")
+def export_data(data, path):
+    with open(path, "w", encoding="utf8") as f:
+        json.dump(data, f, indent=2)
 
-    params, density_grid = np.copy(params), np.copy(density_grid)
-
-    if np.isposinf(params).any():
-        replace = params[~np.isposinf(params)].max()
-        print("+inf in params, replace by", replace)
-        params[np.isposinf(params)] = replace
-    if np.isneginf(params).any():
-        replace = params[~np.isneginf(params)].min()
-        print("-inf in params, replace by", replace)
-        params[np.isneginf(params)] = replace
-
-    if np.isposinf(density_grid).any():
-        replace = density_grid[~np.isposinf(density_grid)].max()
-        print("+inf in density_grid, replace by", replace)
-        density_grid[np.isposinf(density_grid)] = replace
-    if np.isneginf(density_grid).any():
-        replace = density_grid[~np.isneginf(density_grid)].min()
-        print("-inf in density_grid, replace by", replace)
-        density_grid[np.isneginf(density_grid)] = replace
-
-    return params, density_grid
-
-T_TOOBIG = 65500
+def print_data(params_size, params_csr_size, density_grid_size, density_grid_csr_size):
+    print("params", params_size / 1024 / 1024, "MB",
+          "params_csr", params_csr_size / 1024 / 1024, "MB",
+          "density_grid", density_grid_size / 1024 / 1024, "MB",
+          "density_grid_csr", density_grid_csr_size / 1024 / 1024, "MB",)
 
 if __name__ == "__main__":
     import os
@@ -62,12 +37,18 @@ if __name__ == "__main__":
     os.makedirs(os.path.dirname(args.intraexportformat), exist_ok=True)
     savepath = os.path.join(root, args.saveformat % args.start)
 
-    params_sizes, diff_params_sizes = [], []
-    density_grid_sizes, diff_density_grid_sizes = [], []
+    sizes = []
     save = load_save(savepath)
     params, density_grid = load_params(save)
-    params_csr = sparse.csr_matrix(params)
-    density_grid_csr = sparse.csr_matrix(density_grid)
+    params_size, params_csr_size = get_size(params)
+    density_grid_size, density_grid_csr_size = get_size(density_grid)
+    print_data(params_size, params_csr_size, density_grid_size, density_grid_csr_size)
+    export_data(dict(
+        params_size=params_size,
+        params_csr_size=params_csr_size,
+        density_grid_size=density_grid_size,
+        density_grid_csr_size=density_grid_csr_size,
+    ), args.intraexportformat % args.start)
     last_diff_params, last_diff_density_grid = np.copy(params), np.copy(density_grid)
     last_intr_params, last_intr_density_grid = np.copy(params), np.copy(density_grid)
     for i in range(args.start + 1, args.end + 1):
@@ -75,56 +56,28 @@ if __name__ == "__main__":
         savepath = os.path.join(root, args.saveformat % i)
         save = load_save(savepath)
         params, density_grid = load_params(save)
-        params_csr = sparse.csr_matrix(params)
-        density_grid_csr = sparse.csr_matrix(density_grid)
+        params_size, params_csr_size = get_size(params)
+        density_grid_size, density_grid_csr_size = get_size(density_grid)
+        print_data(params_size, params_csr_size, density_grid_size, density_grid_csr_size)
+        export_data(dict(
+            params_size=params_size,
+            params_csr_size=params_csr_size,
+            density_grid_size=density_grid_size,
+            density_grid_csr_size=density_grid_csr_size,
+        ), args.intraexportformat % i)
 
-        diff_density_grid_fp32 = density_grid.astype(np.float32) - last_diff_density_grid.astype(np.float32)
-        diff_density_grid_rel = diff_density_grid_fp32 / last_diff_density_grid.astype(np.float32)
-        diff_density_grid_rel[np.isnan(diff_density_grid_rel)] = diff_density_grid_fp32[np.isnan(diff_density_grid_rel)]
-        density_grid_fp32 = last_diff_density_grid.astype(np.float32) + diff_density_grid_fp32
-        density_grid_error_for_compare = density_grid - (last_diff_density_grid + diff_density_grid_fp32.astype(np.float16))
-
-        diff_params = params - last_diff_params
-        diff_density_grid = density_grid - last_diff_density_grid
-        diff_params[np.abs(diff_params) <= args.T] = 0
-        diff_density_grid[np.abs(diff_density_grid) <= args.T] = 0
-        diff_density_grid[diff_density_grid_fp32 > T_TOOBIG] = T_TOOBIG
-        diff_density_grid[diff_density_grid_fp32 < -T_TOOBIG] = -T_TOOBIG
-        diff_density_grid[density_grid_fp32 > T_TOOBIG] = (T_TOOBIG - last_diff_density_grid)[density_grid_fp32 > T_TOOBIG]
-        diff_density_grid[density_grid_fp32 < -T_TOOBIG] = (-T_TOOBIG - last_diff_density_grid)[density_grid_fp32 < -T_TOOBIG]
-        diff_params_csr = sparse.csr_matrix(diff_params)
-        diff_density_grid_csr = sparse.csr_matrix(diff_density_grid)
+        diff_params = compute_diff_params(params, last_diff_params, args.T)
+        diff_density_grid = compute_diff_density_grid(density_grid, last_diff_density_grid, args.T)
         last_diff_params += diff_params
         last_diff_density_grid += diff_density_grid
-
-        params_sizes.append(params_csr.size)
-        diff_params_sizes.append(diff_params_csr.size)
-        density_grid_sizes.append(density_grid_csr.size)
-        diff_density_grid_sizes.append(diff_density_grid_csr.size)
-        print("params_csr", params_csr.size, "diff_params_csr", diff_params_csr.size)
-        print("density_grid", density_grid_csr.size, "diff_density_grid_csr", diff_density_grid_csr.size)
-
-        error_params = params.astype(np.float32) - last_diff_params.astype(np.float32)
-        error_density_grid = density_grid.astype(np.float32) - last_diff_density_grid.astype(np.float32)
-        print("error of params", error_params.max(), error_params.min())
-        print("error of density_grid", error_density_grid.max(), error_density_grid.min())
-        cause_of_max_error_idx = error_density_grid == error_density_grid.max()
-        cause_of_min_error_idx = error_density_grid == error_density_grid.min()
-        print("cause of density_grid error",
-              error_density_grid.max(),
-              "error should be",
-              density_grid_error_for_compare[cause_of_max_error_idx],
-              density_grid[cause_of_max_error_idx],
-              last_diff_density_grid[cause_of_max_error_idx],
-              diff_density_grid[cause_of_max_error_idx])
-        print("cause of density_grid error",
-              error_density_grid.min(),
-              "error should be",
-              density_grid_error_for_compare[cause_of_min_error_idx],
-              density_grid[cause_of_min_error_idx],
-              last_diff_density_grid[cause_of_min_error_idx],
-              diff_density_grid[cause_of_min_error_idx])
-
-        pass
-    print("params_csr", np.mean(params_sizes), "diff_params_csr", np.mean(diff_params_sizes), "ratio", np.mean(diff_params_sizes)/np.mean(params_sizes))
-    print("density_grid", np.mean(density_grid_sizes), "diff_density_grid_csr", np.mean(diff_density_grid_sizes), "ratio", np.mean(diff_density_grid_sizes)/np.mean(density_grid_sizes))
+        
+        diff_params_size, diff_params_csr_size = get_size(diff_params)
+        diff_density_grid_size, diff_density_grid_csr_size = get_size(diff_density_grid)
+        print("diff:")
+        print_data(diff_params_size, diff_params_csr_size, diff_density_grid_size, diff_density_grid_csr_size)
+        export_data(dict(
+            diff_params_size=diff_params_size,
+            diff_params_csr_size=diff_params_csr_size,
+            diff_density_grid_size=diff_density_grid_size,
+            diff_density_grid_csr_size=diff_density_grid_csr_size,
+        ), args.interexportformat % {'i':i, "T": args.T})
