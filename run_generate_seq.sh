04f10036b4159a39f74f60d1bd6c2c76552a9e09
yindaheng98 <yindaheng98@gmail.com>
2024-01-20 13:08:51 -0800

    run generate at the same time

--- run_generate_seq.sh
+++ run_generate_seq.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-python3 /volume/scripts/parse_seq2bson.py --start 1 --end 101 \
-    --saveformat results/stnerf-taekwondo/frame%d.bson \
-    --intraexportformat results/stnerf-taekwondo/frame%d-intra.bson \
-    --interexportformat results/stnerf-taekwondo/frame%\(i\)dT=%\(T\)fT_density=%\(T_density\)f-inter.bson \
-    --snapshotsimulate_interexportformat results/stnerf-taekwondo/frame%\(i\)dT=%\(T\)fT_density=%\(T_density\)f-snapshotsimulate.bson \
-    --T 0.5 --T_density 0.5
+command() {
+    echo $1 $2 start
+    python3 /volume/scripts/parse_seq2bson.py --start 1 --end 101 \
+        --saveformat results/stnerf-taekwondo/frame%d.bson \
+        --intraexportformat results/stnerf-taekwondo/frame%d-intra.bson \
+        --interexportformat results/stnerf-taekwondo/frame%\(i\)dT=%\(T\)fT_density=%\(T_density\)f-inter.bson \
+        --snapshotsimulate_interexportformat results/stnerf-taekwondo/frame%\(i\)dT=%\(T\)fT_density=%\(T_density\)f-snapshotsimulate.bson \
+        --T $1 --T_density $2
+    echo $1 $2 complete
+}
+
+echo Start Group:
+command 1 1 &
+command 0.5 0.5 &
+command 0.1 0.1 &
+command 0.05 0.05 &
+command 0.01 0.01 &
+wait
