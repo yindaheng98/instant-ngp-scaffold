0534ce49dbc9ccd5ef2cbfcb217fca7e2e70ea02
yindaheng98 <yindaheng98@gmail.com>
2024-01-20 23:05:42 -0800

    eval no freeze

---
+++ run_eval_inter_nofreeze.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+command() {
+    echo $1 $2 start
+    for i in {2..101}
+    do
+    python3 /volume/scripts/eval.py \
+        --load_snapshot results/stnerf-taekwondo/frame${i}-nofreezeT=${1}T_density=${2}-snapshotsimulate.bson \
+        --test_transforms data/nerf/taekwondo/frame${i}/transforms.json
+    done
+    echo $1 $2 complete
+}
+
+command 1.000000 1.000000
+command 0.500000 0.500000
+command 0.100000 0.100000
+command 0.050000 0.050000
+command 0.010000 0.010000
