e17b2b2803370d0565e9ae2ff0c101d15a910ea8
yindaheng98 <yindaheng98@gmail.com>
2024-01-29 19:24:48 -0800

    extract2imgs

--- important_commands.sh
+++ important_commands.sh
@@ -12,3 +12,6 @@ python3 scripts/train_seq.py \
     --no_freeze
 ./instant-ngp-origin --load_snapshot results/stnerf-taekwondo/frame2T=0.100000T_density=0.100000-snapshotsimulate.bson --no-train data/nerf/taekwondo/frame2
 python3 scripts/n3dv2imgs.py --path data/nerf/coffee_martini --exec ffmpeg > temp.sh && cat temp.sh && ./temp.sh
+# python3 scripts/n3dv2imgs.py --path data/nerf/flame_salmon_1 --exec ffmpeg > temp.sh && cat temp.sh && ./temp.sh
+python3 scripts/n3dv2imgs.py --path data/nerf/flame_steak --exec ffmpeg > temp.sh && cat temp.sh && ./temp.sh
+python3 scripts/n3dv2imgs.py --path data/nerf/sear_steak --exec ffmpeg > temp.sh && cat temp.sh && ./temp.sh
