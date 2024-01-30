5a1921ddbc41311925df5f52820dc1e3cc49f480
yindaheng98 <yindaheng98@gmail.com>
2024-01-29 19:21:18 -0800

    better

--- scripts/n3dv2imgs.py
+++ scripts/n3dv2imgs.py
@@ -5,10 +5,12 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--exec", type=str, required=True, help="path to the ffmpeg")
 parser.add_argument("--path", type=str, required=True, help="path to the video folder")
+parser.add_argument("--maxt", type=int, default=4, help="max num of thread")
 
 if __name__ == "__main__":
     args = parser.parse_args()
     root = args.path
+    t = args.maxt
     for entry in os.scandir(root):
         if not re.match(r"cam[0-9][0-9].mp4", entry.name):
             continue
@@ -17,4 +19,8 @@ if __name__ == "__main__":
         os.makedirs(imgs_dir, exist_ok=True)
         cmd = f"{args.exec} -i {entry.path} {imgs_dir}/%03d.png &"
         print(cmd)
+        t -= 1
+        if t <= 0:
+            print("wait")
+            t = args.maxt
     print("wait")
