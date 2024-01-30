7f07ee8d2d94e0ae7b1781e3c24fe49ef8aeabc8
yindaheng98 <yindaheng98@gmail.com>
2024-01-30 15:19:23 -0800

    do all at once

--- run_count_size.sh
+++ run_count_size.sh
@@ -14,26 +14,29 @@ command() {
     python3 /volume/scripts/parse_seq_size.py \
         --start $2 --end $3 \
         --saveformat results/$4/frame%d.bson \
-        --initexportformat results/$4/frame%d-intra.json \
         --intraexportformat results/$4/frame%\(i\)dT=%\(T\)f-intra.json \
         --interexportformat results/$4/frame%\(i\)dT=%\(T\)f-inter.json \
         -T $1
     echo $1 complete
 }
 
-echo Start Group:
-ARGS="1 75 stnerf-walking"
-command0 0 $ARGS &
-command 1 $ARGS &
-command 0.9 $ARGS &
-command 0.8 $ARGS &
-command 0.7 $ARGS &
-command 0.6 $ARGS &
-command 0.5 $ARGS &
-command 0.4 $ARGS &
-command 0.3 $ARGS &
-command 0.2 $ARGS &
-command 0.1 $ARGS &
-command 0.05 $ARGS &
-command 0.01 $ARGS &
-wait
+doall() {
+    ARGS="$1"
+    echo Start Group:
+    command0 0 $ARGS &
+    command 1 $ARGS &
+    command 0.9 $ARGS &
+    command 0.8 $ARGS &
+    command 0.7 $ARGS &
+    command 0.6 $ARGS &
+    command 0.5 $ARGS &
+    command 0.4 $ARGS &
+    command 0.3 $ARGS &
+    command 0.2 $ARGS &
+    command 0.1 $ARGS &
+    command 0.05 $ARGS &
+    command 0.01 $ARGS &
+    wait
+}
+
+doall "1 75 stnerf-walking"
