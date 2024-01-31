0d75f8d76e43a07ca01ade150a1d4e6d7c27431c
yindaheng98 <yindaheng98@gmail.com>
2024-01-31 13:36:16 -0800

    eval all

--- run_eval_inter.sh
+++ run_eval_inter.sh
@@ -10,22 +10,16 @@ command() {
     echo $1 $2 $3 $4 $5 $6 $7 complete
 }
 
-command 1.000000 1.000000 2 10 101 stnerf-walking data/nerf/walking
+doall() {
+    ARGS=$1
+    command 0.500000 0.100000 $ARGS
+    command 0.400000 0.100000 $ARGS
+    command 0.300000 0.100000 $ARGS
+    command 0.200000 0.100000 $ARGS
+    command 0.100000 0.100000 $ARGS
+}
 
-command 1.000000 0.100000 2 10 101 stnerf-walking data/nerf/walking
-
-command 0.500000 0.100000 2 10 101 stnerf-walking data/nerf/walking
-command 0.400000 0.100000 2 10 101 stnerf-walking data/nerf/walking
-command 0.300000 0.100000 2 10 101 stnerf-walking data/nerf/walking
-command 0.200000 0.100000 2 10 101 stnerf-walking data/nerf/walking
-command 0.100000 0.100000 2 10 101 stnerf-walking data/nerf/walking
-
-# command 0.500000 0.050000 2 10 101 stnerf-walking data/nerf/walking
-
-# command 0.050000 0.100000 2 10 101 stnerf-walking data/nerf/walking
-# command 0.100000 0.050000 2 10 101 stnerf-walking data/nerf/walking
-command 0.050000 0.050000 2 10 101 stnerf-walking data/nerf/walking
-
-# command 0.010000 0.100000 2 10 101 stnerf-walking data/nerf/walking
-command 0.100000 0.010000 2 10 101 stnerf-walking data/nerf/walking
-command 0.010000 0.010000 2 10 101 stnerf-walking data/nerf/walking
+doall "2 5 75 stnerf-walking data/nerf/walking"
+doall "2 5 100 coffee_martini data/nerf/coffee_martini"
+doall "2 5 100 flame_steak data/nerf/flame_steak"
+doall "2 5 100 sear_steak data/nerf/sear_steak"
