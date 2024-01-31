a4e7b68e5085c92abe49fa0972a701ee12cb5df7
yindaheng98 <yindaheng98@gmail.com>
2024-01-31 13:12:51 -0800

    train them all

---
+++ train_all.sh
@@ -0,0 +1,20 @@
+python3 scripts/train_seq.py \
+    --init_steps 10000 --steps 10000 --start 1 --end 75 --decay 0.5 \
+    --dataformat "data/nerf/walking/frame%d" \
+    --saveformat "results/stnerf-walking/frame%d.bson" \
+    --executable "./instant-ngp-train"
+python3 scripts/train_seq.py \
+    --init_steps 30000 --steps 10000 --start 1 --end 300 --decay 0.5 \
+    --dataformat "data/nerf/coffee_martini/frame%03d" \
+    --saveformat "results/coffee_martini/frame%d.bson" \
+    --executable "./instant-ngp-train"
+python3 scripts/train_seq.py \
+    --init_steps 30000 --steps 10000 --start 1 --end 300 --decay 0.5 \
+    --dataformat "data/nerf/flame_steak/frame%03d" \
+    --saveformat "results/flame_steak/frame%d.bson" \
+    --executable "./instant-ngp-train"
+python3 scripts/train_seq.py \
+    --init_steps 30000 --steps 10000 --start 1 --end 300 --decay 0.5 \
+    --dataformat "data/nerf/sear_steak/frame%03d" \
+    --saveformat "results/sear_steak/frame%d.bson" \
+    --executable "./instant-ngp-train"
