14bf5e81f6fd822b4a175d59e0864d9b69eb5a29
yindaheng98 <yindaheng98@gmail.com>
2024-01-31 13:26:00 -0800

    count every 10 frames

--- run_eval_inter.sh
+++ run_eval_inter.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 command() {
-    echo $1 $2 start
-    for i in {2..101}
+    echo $1 $2 $3 $4 $5 $6 $7 start
+    for i in `seq $3 $4 $5`
     do
     python3 /volume/scripts/eval.py \
-        --load_snapshot results/stnerf-taekwondo/frame${i}T=${1}T_density=${2}-snapshotsimulate.bson \
-        --test_transforms data/nerf/taekwondo/frame${i}/transforms.json
+        --load_snapshot results/$6/frame${i}T=${1}T_density=${2}-snapshotsimulate.bson \
+        --test_transforms $7/frame${i}/transforms.json
     done
-    echo $1 $2 complete
+    echo $1 $2 $3 $4 $5 $6 $7 complete
 }
 
-command 1.000000 1.000000
+command 1.000000 1.000000 2 10 101 stnerf-walking data/nerf/walking
 
-command 1.000000 0.100000
+command 1.000000 0.100000 2 10 101 stnerf-walking data/nerf/walking
 
-command 0.500000 0.100000
-command 0.400000 0.100000
-command 0.300000 0.100000
-command 0.200000 0.100000
-command 0.100000 0.100000
+command 0.500000 0.100000 2 10 101 stnerf-walking data/nerf/walking
+command 0.400000 0.100000 2 10 101 stnerf-walking data/nerf/walking
+command 0.300000 0.100000 2 10 101 stnerf-walking data/nerf/walking
+command 0.200000 0.100000 2 10 101 stnerf-walking data/nerf/walking
+command 0.100000 0.100000 2 10 101 stnerf-walking data/nerf/walking
 
-# command 0.500000 0.050000
+# command 0.500000 0.050000 2 10 101 stnerf-walking data/nerf/walking
 
-# command 0.050000 0.100000
-# command 0.100000 0.050000
-command 0.050000 0.050000
+# command 0.050000 0.100000 2 10 101 stnerf-walking data/nerf/walking
+# command 0.100000 0.050000 2 10 101 stnerf-walking data/nerf/walking
+command 0.050000 0.050000 2 10 101 stnerf-walking data/nerf/walking
 
-# command 0.010000 0.100000
-command 0.100000 0.010000
-command 0.010000 0.010000
+# command 0.010000 0.100000 2 10 101 stnerf-walking data/nerf/walking
+command 0.100000 0.010000 2 10 101 stnerf-walking data/nerf/walking
+command 0.010000 0.010000 2 10 101 stnerf-walking data/nerf/walking
