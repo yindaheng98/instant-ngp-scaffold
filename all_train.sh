98f64e6b0ec38e97a6b9adf899d50a0b3871aa69
yindaheng98 <yindaheng98@gmail.com>
2024-02-09 21:22:14 -0800

    train all 1000 steps

--- all_train.sh
+++ all_train.sh
@@ -29,17 +29,46 @@ python3 scripts/train_seq.py \
     --executable "./instant-ngp-train"
 
 python3 scripts/train_seq.py \
-    --init_steps 30000 --steps 10000 --start 1 --end 300 --decay 0.5 \
+    --init_steps 30000 --steps 1000 --start 1 --end 300 \
+    --trainargs=--decay=0.5 \
+    --initformat "data/nerf/coffee_martini/frame%d" \
+    --dataformat "data/nerf/coffee_martini/residuals/frame%d" \
+    --saveformat "results/coffee_martini-partial/frame%d.bson" \
+    --executable "./instant-ngp-train"
+python3 scripts/train_seq.py \
+    --init_steps 30000 --steps 1000 --start 1 --end 300 \
+    --trainargs=--decay=0.5 \
+    --initformat "data/nerf/coffee_martini/frame%d" \
     --dataformat "data/nerf/coffee_martini/frame%d" \
-    --saveformat "results/coffee_martini/frame%d.bson" \
+    --saveformat "results/coffee_martini-1000steps/frame%d.bson" \
+    --executable "./instant-ngp-train"
+
+python3 scripts/train_seq.py \
+    --init_steps 30000 --steps 1000 --start 1 --end 300 \
+    --trainargs=--decay=0.5 \
+    --initformat "data/nerf/flame_steak/frame%d" \
+    --dataformat "data/nerf/flame_steak/residuals/frame%d" \
+    --saveformat "results/flame_steak-partial/frame%d.bson" \
     --executable "./instant-ngp-train"
 python3 scripts/train_seq.py \
-    --init_steps 30000 --steps 10000 --start 1 --end 300 --decay 0.5 \
+    --init_steps 30000 --steps 1000 --start 1 --end 300 \
+    --trainargs=--decay=0.5 \
+    --initformat "data/nerf/flame_steak/frame%d" \
     --dataformat "data/nerf/flame_steak/frame%d" \
-    --saveformat "results/flame_steak/frame%d.bson" \
+    --saveformat "results/flame_steak-1000steps/frame%d.bson" \
+    --executable "./instant-ngp-train"
+
+python3 scripts/train_seq.py \
+    --init_steps 30000 --steps 1000 --start 1 --end 300 \
+    --trainargs=--decay=0.5 \
+    --initformat "data/nerf/sear_steak/frame%d" \
+    --dataformat "data/nerf/sear_steak/residuals/frame%d" \
+    --saveformat "results/sear_steak-partial/frame%d.bson" \
     --executable "./instant-ngp-train"
 python3 scripts/train_seq.py \
-    --init_steps 30000 --steps 10000 --start 1 --end 300 --decay 0.5 \
+    --init_steps 30000 --steps 1000 --start 1 --end 300 \
+    --trainargs=--decay=0.5 \
+    --initformat "data/nerf/sear_steak/frame%d" \
     --dataformat "data/nerf/sear_steak/frame%d" \
-    --saveformat "results/sear_steak/frame%d.bson" \
+    --saveformat "results/sear_steak-1000steps/frame%d.bson" \
     --executable "./instant-ngp-train"
