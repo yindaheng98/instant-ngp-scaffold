0e9f3d2413e4b08fcce3dde6341b8fc57b7e77ce
yindaheng98 <yindaheng98@gmail.com>
2024-02-09 17:45:01 -0800

    train 1000 steps

--- all_train.sh
+++ all_train.sh
@@ -1,8 +1,33 @@
 python3 scripts/train_seq.py \
-    --init_steps 10000 --steps 10000 --start 1 --end 75 --decay 0.5 \
-    --dataformat "data/nerf/walking/frame%d" \
-    --saveformat "results/stnerf-walking/frame%d.bson" \
+    --init_steps 10000 --steps 1000 --start 1 --end 101 \
+    --trainargs=--decay=0.5 \
+    --initformat "data/nerf/taekwondo/frame%d" \
+    --dataformat "data/nerf/taekwondo/residuals/frame%d" \
+    --saveformat "results/stnerf-taekwondo-partial/frame%d.bson" \
     --executable "./instant-ngp-train"
+python3 scripts/train_seq.py \
+    --init_steps 10000 --steps 1000 --start 1 --end 101 \
+    --trainargs=--decay=0.5 \
+    --initformat "data/nerf/taekwondo/frame%d" \
+    --dataformat "data/nerf/taekwondo/frame%d" \
+    --saveformat "results/stnerf-taekwondo-1000steps/frame%d.bson" \
+    --executable "./instant-ngp-train"
+
+python3 scripts/train_seq.py \
+    --init_steps 10000 --steps 1000 --start 1 --end 75 \
+    --trainargs=--decay=0.5 \
+    --initformat "data/nerf/walking/frame%d" \
+    --dataformat "data/nerf/walking/residuals/frame%d" \
+    --saveformat "results/stnerf-walking-1000steps/frame%d.bson" \
+    --executable "./instant-ngp-train"
+python3 scripts/train_seq.py \
+    --init_steps 10000 --steps 1000 --start 1 --end 75 \
+    --trainargs=--decay=0.5 \
+    --initformat "data/nerf/walking/frame%d" \
+    --dataformat "data/nerf/walking/frame%d" \
+    --saveformat "results/stnerf-walking-partial/frame%d.bson" \
+    --executable "./instant-ngp-train"
+
 python3 scripts/train_seq.py \
     --init_steps 30000 --steps 10000 --start 1 --end 300 --decay 0.5 \
     --dataformat "data/nerf/coffee_martini/frame%d" \
