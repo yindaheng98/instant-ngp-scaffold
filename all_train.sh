12f6b1e7530b985ba98f69500854371f128b5fb3
yindaheng98 <yindaheng98@gmail.com>
2024-02-25 16:02:19 -0800

    use residual_regulization

--- all_train.sh
+++ all_train.sh
@@ -1,6 +1,7 @@
 python3 scripts/train_seq.py \
     --init_steps 10000 --steps 1000 --start 1 --end 101 \
     --trainargs=--decay=0.5 \
+    --trainargs=--encreg=1e-4 \
     --initformat "data/nerf/taekwondo/frame%d" \
     --dataformat "data/nerf/taekwondo/residuals/frame%d" \
     --saveformat "results/stnerf-taekwondo-partial/frame%d.bson" \
@@ -8,6 +9,7 @@ python3 scripts/train_seq.py \
 python3 scripts/train_seq.py \
     --init_steps 10000 --steps 1000 --start 1 --end 101 \
     --trainargs=--decay=0.5 \
+    --trainargs=--encreg=1e-4 \
     --initformat "data/nerf/taekwondo/frame%d" \
     --dataformat "data/nerf/taekwondo/frame%d" \
     --saveformat "results/stnerf-taekwondo-1000steps/frame%d.bson" \
@@ -16,6 +18,7 @@ python3 scripts/train_seq.py \
 python3 scripts/train_seq.py \
     --init_steps 10000 --steps 1000 --start 1 --end 75 \
     --trainargs=--decay=0.5 \
+    --trainargs=--encreg=1e-4 \
     --initformat "data/nerf/walking/frame%d" \
     --dataformat "data/nerf/walking/residuals/frame%d" \
     --saveformat "results/stnerf-walking-partial/frame%d.bson" \
@@ -23,6 +26,7 @@ python3 scripts/train_seq.py \
 python3 scripts/train_seq.py \
     --init_steps 10000 --steps 1000 --start 1 --end 75 \
     --trainargs=--decay=0.5 \
+    --trainargs=--encreg=1e-4 \
     --initformat "data/nerf/walking/frame%d" \
     --dataformat "data/nerf/walking/frame%d" \
     --saveformat "results/stnerf-walking-1000steps/frame%d.bson" \
@@ -31,6 +35,7 @@ python3 scripts/train_seq.py \
 python3 scripts/train_seq.py \
     --init_steps 30000 --steps 1000 --start 1 --end 300 \
     --trainargs=--decay=0.5 \
+    --trainargs=--encreg=1e-4 \
     --initformat "data/nerf/coffee_martini/frame%d" \
     --dataformat "data/nerf/coffee_martini/residuals/frame%d" \
     --saveformat "results/coffee_martini-partial/frame%d.bson" \
@@ -38,6 +43,7 @@ python3 scripts/train_seq.py \
 python3 scripts/train_seq.py \
     --init_steps 30000 --steps 1000 --start 1 --end 300 \
     --trainargs=--decay=0.5 \
+    --trainargs=--encreg=1e-4 \
     --initformat "data/nerf/coffee_martini/frame%d" \
     --dataformat "data/nerf/coffee_martini/frame%d" \
     --saveformat "results/coffee_martini-1000steps/frame%d.bson" \
@@ -46,6 +52,7 @@ python3 scripts/train_seq.py \
 python3 scripts/train_seq.py \
     --init_steps 30000 --steps 1000 --start 1 --end 300 \
     --trainargs=--decay=0.5 \
+    --trainargs=--encreg=1e-4 \
     --initformat "data/nerf/flame_steak/frame%d" \
     --dataformat "data/nerf/flame_steak/residuals/frame%d" \
     --saveformat "results/flame_steak-partial/frame%d.bson" \
@@ -53,6 +60,7 @@ python3 scripts/train_seq.py \
 python3 scripts/train_seq.py \
     --init_steps 30000 --steps 1000 --start 1 --end 300 \
     --trainargs=--decay=0.5 \
+    --trainargs=--encreg=1e-4 \
     --initformat "data/nerf/flame_steak/frame%d" \
     --dataformat "data/nerf/flame_steak/frame%d" \
     --saveformat "results/flame_steak-1000steps/frame%d.bson" \
@@ -61,6 +69,7 @@ python3 scripts/train_seq.py \
 python3 scripts/train_seq.py \
     --init_steps 30000 --steps 1000 --start 1 --end 300 \
     --trainargs=--decay=0.5 \
+    --trainargs=--encreg=1e-4 \
     --initformat "data/nerf/sear_steak/frame%d" \
     --dataformat "data/nerf/sear_steak/residuals/frame%d" \
     --saveformat "results/sear_steak-partial/frame%d.bson" \
@@ -68,6 +77,7 @@ python3 scripts/train_seq.py \
 python3 scripts/train_seq.py \
     --init_steps 30000 --steps 1000 --start 1 --end 300 \
     --trainargs=--decay=0.5 \
+    --trainargs=--encreg=1e-4 \
     --initformat "data/nerf/sear_steak/frame%d" \
     --dataformat "data/nerf/sear_steak/frame%d" \
     --saveformat "results/sear_steak-1000steps/frame%d.bson" \
