47fb67025de641726663394904ce3643b31ba7b7
yindaheng98 <yindaheng98@gmail.com>
2024-02-28 15:31:41 -0800

    0也一视同仁

--- run_count_size.sh
+++ run_count_size.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-command0() {
-    echo 0 start
-    python ./scripts/parse_seq_size.py \
-        --start $2 --end $3 \
-        --saveformat results/$4/frame%d.bson \
-        --initexportformat results/$4/frame%d-intra.json \
-        -T 0
-    echo 0 complete
-}
-
 command() {
     echo $1 start
     python ./scripts/parse_seq_size.py \
         --start $2 --end $3 \
         --saveformat results/$4/frame%d.bson \
-        --intraexportformat results/$4/frame%\(i\)dT=%\(T\)f-intra.json \
-        --interexportformat results/$4/frame%\(i\)dT=%\(T\)f-inter.json \
+        --intraexportformat results/$4/size/frame%\(i\)dT=%\(T\)f-intra.json \
+        --interexportformat results/$4/size/frame%\(i\)dT=%\(T\)f-inter.json \
         -T $1
     echo $1 complete
 }
@@ -23,12 +13,7 @@ command() {
 doall() {
     ARGS="$1"
     echo Start Group:
-    command0 0 $ARGS &
-    command 1 $ARGS &
-    command 0.9 $ARGS &
-    command 0.8 $ARGS &
-    command 0.7 $ARGS &
-    command 0.6 $ARGS &
+    command 0 $ARGS &
     command 0.5 $ARGS &
     command 0.4 $ARGS &
     command 0.3 $ARGS &
@@ -40,7 +25,17 @@ doall() {
 }
 
 doall "1 101 stnerf-taekwondo-regularization-1e-6"
+doall "1 101 stnerf-taekwondo-regularization-1e-5"
+doall "1 101 stnerf-taekwondo-regularization-1e-4"
 doall "1 75 stnerf-walking-regularization-1e-6"
+doall "1 75 stnerf-walking-regularization-1e-5"
+doall "1 75 stnerf-walking-regularization-1e-4"
 doall "1 100 coffee_martini-regularization-1e-6"
+doall "1 100 coffee_martini-regularization-1e-5"
+doall "1 100 coffee_martini-regularization-1e-4"
 doall "1 100 flame_steak-regularization-1e-6"
+doall "1 100 flame_steak-regularization-1e-5"
+doall "1 100 flame_steak-regularization-1e-4"
 doall "1 100 sear_steak-regularization-1e-6"
+doall "1 100 sear_steak-regularization-1e-5"
+doall "1 100 sear_steak-regularization-1e-4"
