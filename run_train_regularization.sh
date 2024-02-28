67587d17f59c353bd46b54fa4ba989a64ee35aa5
yindaheng98 <yindaheng98@gmail.com>
2024-02-27 16:13:13 -0800

    train them all

--- run_train_regularization.sh
+++ run_train_regularization.sh
@@ -10,3 +10,21 @@ command() {
 }
 
 command taekwondo stnerf-taekwondo "1e-4" 30000 10000 1 101
+command taekwondo stnerf-taekwondo "1e-5" 30000 10000 1 101
+command taekwondo stnerf-taekwondo "1e-6" 30000 10000 1 101
+
+command walking stnerf-walking "1e-4" 30000 10000 1 75
+command walking stnerf-walking "1e-5" 30000 10000 1 75
+command walking stnerf-walking "1e-6" 30000 10000 1 75
+
+command coffee_martini coffee_martini "1e-4" 30000 10000 1 300
+command coffee_martini coffee_martini "1e-5" 30000 10000 1 300
+command coffee_martini coffee_martini "1e-6" 30000 10000 1 300
+
+command flame_steak flame_steak "1e-4" 30000 10000 1 300
+command flame_steak flame_steak "1e-5" 30000 10000 1 300
+command flame_steak flame_steak "1e-6" 30000 10000 1 300
+
+command sear_steak sear_steak "1e-4" 30000 10000 1 300
+command sear_steak sear_steak "1e-5" 30000 10000 1 300
+command sear_steak sear_steak "1e-6" 30000 10000 1 300
