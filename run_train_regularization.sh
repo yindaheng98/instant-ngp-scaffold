872b3b6cabec6c040d317466f4d86c8f6e264d06
yindaheng98 <yindaheng98@gmail.com>
2024-02-28 14:52:11 -0800

    we need 1e-7

--- run_train_regularization.sh
+++ run_train_regularization.sh
@@ -34,27 +34,32 @@ command1 taekwondo stnerf-taekwondo "none" 30000 10000 1 101
 command taekwondo stnerf-taekwondo "1e-4" 30000 10000 1 101
 command taekwondo stnerf-taekwondo "1e-5" 30000 10000 1 101
 command taekwondo stnerf-taekwondo "1e-6" 30000 10000 1 101
+command taekwondo stnerf-taekwondo "1e-7" 30000 10000 1 101
 
 command0 walking stnerf-walking 30000 1
 command1 taekwondo stnerf-walking "none" 30000 10000 1 75
 command walking stnerf-walking "1e-4" 30000 10000 1 75
 command walking stnerf-walking "1e-5" 30000 10000 1 75
 command walking stnerf-walking "1e-6" 30000 10000 1 75
+command walking stnerf-walking "1e-7" 30000 10000 1 75
 
 command0 coffee_martini coffee_martini 30000 1
 command1 coffee_martini coffee_martini "none" 30000 10000 1 300
 command coffee_martini coffee_martini "1e-4" 30000 10000 1 300
 command coffee_martini coffee_martini "1e-5" 30000 10000 1 300
 command coffee_martini coffee_martini "1e-6" 30000 10000 1 300
+command coffee_martini coffee_martini "1e-7" 30000 10000 1 300
 
 command0 flame_steak flame_steak 30000 1
 command1 flame_steak flame_steak "none" 30000 10000 1 300
 command flame_steak flame_steak "1e-4" 30000 10000 1 300
 command flame_steak flame_steak "1e-5" 30000 10000 1 300
 command flame_steak flame_steak "1e-6" 30000 10000 1 300
+command flame_steak flame_steak "1e-7" 30000 10000 1 300
 
 command0 sear_steak sear_steak 30000 1
 command1 sear_steak sear_steak "none" 30000 10000 1 300
 command sear_steak sear_steak "1e-4" 30000 10000 1 300
 command sear_steak sear_steak "1e-5" 30000 10000 1 300
 command sear_steak sear_steak "1e-6" 30000 10000 1 300
+command sear_steak sear_steak "1e-7" 30000 10000 1 300
