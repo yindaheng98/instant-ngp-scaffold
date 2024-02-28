b31cddfa4db1d908221a077184a2a241a4baee4b
yindaheng98 <yindaheng98@gmail.com>
2024-02-27 16:24:52 -0800

    unify 1st frame

--- run_train_regularization.sh
+++ run_train_regularization.sh
@@ -1,4 +1,11 @@
+command0() {
+    ./instant-ngp-train \
+        --step=$3 \
+        --save_snapshot results/$2-frame0.bson \
+        data/nerf/$1/frame$4
+}
 command() {
+    cp results/$2-frame0.bson results/$2-regularization-$3/frame$6.bson
     python scripts/train_seq.py \
         --init_steps $4 --steps $5 --start $6 --end $7 \
         --trainargs=--decay=0.5 \
@@ -9,22 +16,27 @@ command() {
         --executable "./instant-ngp-train"
 }
 
+command0 taekwondo stnerf-taekwondo 30000 1
 command taekwondo stnerf-taekwondo "1e-4" 30000 10000 1 101
 command taekwondo stnerf-taekwondo "1e-5" 30000 10000 1 101
 command taekwondo stnerf-taekwondo "1e-6" 30000 10000 1 101
 
+command0 walking stnerf-walking 30000 1
 command walking stnerf-walking "1e-4" 30000 10000 1 75
 command walking stnerf-walking "1e-5" 30000 10000 1 75
 command walking stnerf-walking "1e-6" 30000 10000 1 75
 
+command0 coffee_martini coffee_martini 30000 1
 command coffee_martini coffee_martini "1e-4" 30000 10000 1 300
 command coffee_martini coffee_martini "1e-5" 30000 10000 1 300
 command coffee_martini coffee_martini "1e-6" 30000 10000 1 300
 
+command0 flame_steak flame_steak 30000 1
 command flame_steak flame_steak "1e-4" 30000 10000 1 300
 command flame_steak flame_steak "1e-5" 30000 10000 1 300
 command flame_steak flame_steak "1e-6" 30000 10000 1 300
 
+command0 sear_steak sear_steak 30000 1
 command sear_steak sear_steak "1e-4" 30000 10000 1 300
 command sear_steak sear_steak "1e-5" 30000 10000 1 300
 command sear_steak sear_steak "1e-6" 30000 10000 1 300
