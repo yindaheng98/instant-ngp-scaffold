a824ebabca39bb05a0c2c40409de87dc18ca5905
yindaheng98 <yindaheng98@gmail.com>
2024-02-28 00:50:13 -0800

    bug fixed

--- run_train_regularization.sh
+++ run_train_regularization.sh
@@ -5,6 +5,7 @@ command0() {
         data/nerf/$1/frame$4
 }
 command() {
+    mkdir -p results/$2-frame0.bson results/$2-regularization-$3
     cp results/$2-frame0.bson results/$2-regularization-$3/frame$6.bson
     python scripts/train_seq.py \
         --init_steps $4 --steps $5 --start $6 --end $7 \
