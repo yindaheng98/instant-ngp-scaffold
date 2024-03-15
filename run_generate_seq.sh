a1fa173ffae96194a86ee1740e3ebb83376279d4
yindaheng98 <yindaheng98@gmail.com>
2024-03-14 20:32:47 -0700

    no more destroy

--- run_generate_seq.sh
+++ run_generate_seq.sh
@@ -7,18 +7,6 @@ command0() {
     echo $1 $2 $3 $4 $5 complete
 }
 
-command_destroy() {
-    echo $1 $2 $3 $4 $5 start
-    python ./scripts/parse_seq2bson_destroyed.py --start $3 --end $4 \
-        --saveformat results/$5/frame%d.bson \
-        --exportformat results/$5/fp16destroy/frame%d.bson
-    python ./scripts/parse_seq2bson_no_simulation.py --start $3 --end $4 \
-        --saveformat results/$5/frame%d.bson \
-        --exportformat results/$5/nosimulationdestroy/T=%\(T\)fT_density=%\(T_density\)f/frame%\(i\)d.bson \
-        --T $1 --T_density $2
-    echo $1 $2 $3 $4 $5 complete
-}
-
 command() {
     echo $1 $2 $3 $4 $5 start
     python ./scripts/parse_seq2bson.py --start $3 --end $4 \
@@ -34,15 +22,9 @@ doall() {
     # 1 0.5 0.1 0.05 0.01
     ARGS=$1
     command0 0 0 $ARGS &
-    command_destroy 0.1 0.1 $ARGS &
     # command 1 1 $ARGS &
 
     # command 1 0.1 $ARGS &
-
-    command 0.5 0.1 $ARGS &
-    command 0.4 0.1 $ARGS &
-    command 0.3 0.1 $ARGS &
-    command 0.2 0.1 $ARGS &
     command 0.1 0.1 $ARGS &
 
     # command 0.5 0.05 &
