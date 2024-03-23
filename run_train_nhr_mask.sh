f6ba634634045023b985917fe54c5f7a2acb4a20
yindaheng98 <yindaheng98@gmail.com>
2024-03-23 16:54:07 -0700

    test the mask

---
+++ run_train_nhr_mask.sh
@@ -0,0 +1,4 @@
+./instant-ngp-train --step=30000 --save_snapshot results/basketball-base16-frame0.bson data/nerf/basketball/img/0
+./instant-ngp-train --step=30000 --save_snapshot results/basketball-base16-mask-frame0.bson data/nerf/basketball/img/0/mask
+./instant-ngp-train --step=30000 --save_snapshot results/basketball-base15-frame0.bson data/nerf/basketball/img/0
+./instant-ngp-train --step=30000 --save_snapshot results/basketball-base15-mask-frame0.bson data/nerf/basketball/img/0/mask
