bb6c17e6d0fbe72ba46ce060d660947b23341c32
yindaheng98 <yindaheng98@gmail.com>
2024-03-23 18:07:51 -0700

    bug fixed

--- run_train_nhr_mask.sh
+++ run_train_nhr_mask.sh
@@ -1,7 +1,7 @@
-./instant-ngp-train --step=30000 -c configs/nerf/base_16 --save_snapshot results/basketball-base_16-frame0.bson data/nerf/basketball/img/0
-./instant-ngp-train --step=30000 -c configs/nerf/base_16 --save_snapshot results/basketball-base_16-mask-frame0.bson data/nerf/basketball/img/0/mask
-./instant-ngp-train --step=30000 -c configs/nerf/base_15 --save_snapshot results/basketball-base_15-frame0.bson data/nerf/basketball/img/0
-./instant-ngp-train --step=30000 -c configs/nerf/base_15 --save_snapshot results/basketball-base_15-mask-frame0.bson data/nerf/basketball/img/0/mask
+./instant-ngp-train --step=30000 -c configs/nerf/base_16.json --save_snapshot results/basketball-base_16-frame0.bson data/nerf/basketball/img/0
+./instant-ngp-train --step=30000 -c configs/nerf/base_16.json --save_snapshot results/basketball-base_16-mask-frame0.bson data/nerf/basketball/img/0/mask
+./instant-ngp-train --step=30000 -c configs/nerf/base_15.json --save_snapshot results/basketball-base_15-frame0.bson data/nerf/basketball/img/0
+./instant-ngp-train --step=30000 -c configs/nerf/base_15.json --save_snapshot results/basketball-base_15-mask-frame0.bson data/nerf/basketball/img/0/mask
 python scripts/eval.py --load_snapshot results/basketball-base_16-frame0.bson --write_image results/eval_images/basketball-base_16-frame0 --test_transforms data/nerf/basketball/img/0/transforms.json
 python scripts/eval.py --load_snapshot results/basketball-base_16-mask-frame0.bson --write_image results/eval_images/basketball-base_16-mask-frame0 --test_transforms data/nerf/basketball/img/0/mask/transforms.json
 python scripts/eval.py --load_snapshot results/basketball-base_15-frame0.bson --write_image results/eval_images/basketball-base_15-frame0 --test_transforms data/nerf/basketball/img/0/transforms.json
