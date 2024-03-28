c92fb8651f3810659829f39416d2fde68615e870
yindaheng98 <yindaheng98@gmail.com>
2024-03-28 16:53:04 -0700

    gen it

--- run_generate_seq.sh
+++ run_generate_seq.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 command0() {
-    echo $1 $2 $3 $4 $5 start
-    python ./scripts/parse_seq2bson.py --start $3 --end $4 \
-        --saveformat results/$5/frame%d.bson \
-        --intraexportformat results/$5/intra/frame%d.bson
-    echo $1 $2 $3 $4 $5 complete
+    echo $1  $2 $3 $4 start
+    python ./scripts/parse_seq2bson.py --start $2 --end $3 \
+        --saveformat results/$4/frame%d.bson \
+        --intraexportformat results/$4/intra/frame%d.bson
+    echo $1 $2 $3 $4 complete
 }
 
 command() {
-    echo $1 $2 $3 $4 $5 start
-    python ./scripts/parse_seq2bson.py --start $3 --end $4 \
-        --saveformat results/$5/frame%d.bson \
-        --interexportformat results/$5/inter/T=%\(T\)fT_density=%\(T_density\)f/frame%\(i\)d.bson \
-        --snapshotsimulate_interexportformat results/$5/inter/T=%\(T\)fT_density=%\(T_density\)f/frame%\(i\)d-snapshotsimulate.bson \
-        --T $1 --T_density $2
-    echo $1 $2 $3 $4 $5 complete
+    echo $1  $2 $3 $4 start
+    python ./scripts/parse_seq2bson.py --start $2 --end $3 \
+        --saveformat results/$4/frame%d.bson \
+        --interexportformat results/$4/inter/T=%\(T\)f/frame%\(i\)d.bson \
+        --snapshotsimulate_interexportformat results/$4/inter/T=%\(T\)f/frame%\(i\)d-snapshotsimulate.bson \
+        -T $1
+    echo $1 $2 $3 $4 complete
 }
 
 doall() {
     echo Start Group:
     # 1 0.5 0.1 0.05 0.01
     ARGS=$1
-    command0 0 0 $ARGS &
-    command 0.1 0 $ARGS &
-    command 0.05 0 $ARGS &
-    command 0.01 0 $ARGS &
+    command0 0 $ARGS &
+    command 0.1 $ARGS &
+    command 0.05 $ARGS &
+    command 0.01 $ARGS &
     wait
 }
 
-doall "1 101 stnerf-taekwondo"
-doall "1 75 stnerf-walking"
-doall "1 100 coffee_martini"
-doall "1 100 flame_steak"
-doall "1 100 sear_steak"
+doall "1 101 stnerf-taekwondo-regularization-1e-6"
+# doall "1 75 stnerf-walking"
+# doall "1 100 coffee_martini"
+# doall "1 100 flame_steak"
+# doall "1 100 sear_steak"
