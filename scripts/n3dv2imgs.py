dc5b9aafc34a68f01614b1f1aaa5d8b1a59e790b
yindaheng98 <yindaheng98@gmail.com>
2024-04-05 15:41:18 -0700

    meeting room to images

--- scripts/n3dv2imgs.py
+++ scripts/n3dv2imgs.py
@@ -6,13 +6,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--exec", type=str, required=True, help="path to the ffmpeg")
 parser.add_argument("--path", type=str, required=True, help="path to the video folder")
 parser.add_argument("--maxt", type=int, default=4, help="max num of thread")
+parser.add_argument("--fmt", type=str, default=r"cam[0-9][0-9].mp4", help="re format of mp4 file")
 
 if __name__ == "__main__":
     args = parser.parse_args()
     root = args.path
     t = args.maxt
     for entry in os.scandir(root):
-        if not re.match(r"cam[0-9][0-9].mp4", entry.name):
+        if not re.match(args.fmt, entry.name):
             continue
         folder = os.path.splitext(entry.name)[0]
         imgs_dir = os.path.join(root, folder)
