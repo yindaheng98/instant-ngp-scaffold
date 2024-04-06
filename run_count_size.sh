d5a65ed8186e179ccfd4bd5532a6d1f34df09045
yindaheng98 <yindaheng98@gmail.com>
2024-04-05 21:52:34 -0700

    count the levels

--- run_count_size.sh
+++ run_count_size.sh
@@ -5,23 +5,18 @@ command() {
         --start $2 --end $3 \
         --saveformat results/$4/frame%d.bson \
         --fullexportformat results/$4/size/frame%d.json \
-        --intraexportformat results/$4/size/frame%\(i\)dT=%\(T\)f-intra.json \
-        --interexportformat results/$4/size/frame%\(i\)dT=%\(T\)f-inter.json \
-        -T $1
+        --interexportformat results/$4/size/frame%\(i\)dT=%\(T\)fL=%\(L\)s-inter.json \
+        -T 0.01 -L $1
     echo $1 complete
 }
 
 doall() {
     ARGS="$1"
     echo Start Group:
-    command 0 $ARGS &
-    command 0.5 $ARGS &
-    command 0.4 $ARGS &
-    command 0.3 $ARGS &
     command 0.2 $ARGS &
-    command 0.1 $ARGS &
-    command 0.05 $ARGS &
-    command 0.01 $ARGS &
+    command 0.4 $ARGS &
+    command 0.6 $ARGS &
+    command 0.8 $ARGS &
     wait
 }
 
