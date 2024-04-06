1159fba6836b9c6275cd8130e0f41db1cd9a03b6
yindaheng98 <yindaheng98@gmail.com>
2024-04-05 21:04:56 -0700

    count params only

--- scripts/parse_seq_size.py
+++ scripts/parse_seq_size.py
@@ -49,12 +49,10 @@ def export_data(data, path):
     with open(path, "w", encoding="utf8") as f:
         json.dump(data, f, indent=2)
 
-def print_data(perfix, params_size, params_csr_size, density_grid_size, density_grid_csr_size):
+def print_data(perfix, params_size, params_csr_size):
     print(perfix, 
           "params", params_size / 1024 / 1024, "MB",
-          "params_csr", params_csr_size / 1024 / 1024, "MB",
-          "density_grid", density_grid_size / 1024 / 1024, "MB",
-          "density_grid_csr", density_grid_csr_size / 1024 / 1024, "MB",)
+          "params_csr", params_csr_size / 1024 / 1024, "MB",)
 
 if __name__ == "__main__":
     import os
@@ -64,64 +62,49 @@ if __name__ == "__main__":
 
     sizes = []
     save = load_save(savepath)
-    params, density_grid = load_params(save)
+    params, _ = load_params(save)
     if args.initexportformat:
         os.makedirs(os.path.dirname(args.initexportformat), exist_ok=True)
         params_size, params_csr_size = get_size(params)
-        density_grid_size, density_grid_csr_size = get_size(density_grid)
-        print_data(f"{args.T}  init", params_size, params_csr_size, density_grid_size, density_grid_csr_size)
+        print_data(f"{args.T}  init", params_size, params_csr_size)
         export_data(dict(
             params_size=params_size,
             params_csr_size=params_csr_size,
-            density_grid_size=density_grid_size,
-            density_grid_csr_size=density_grid_csr_size,
         ), args.initexportformat % args.start)
-    last_diff_params, last_diff_density_grid = np.copy(params), np.copy(density_grid)
-    last_intr_params, last_intr_density_grid = np.copy(params), np.copy(density_grid)
+    last_diff_params = np.copy(params)
+    last_intr_params = np.copy(params)
     for i in range(args.start + 1, args.end + 1):
         print("do", i-args.start, "/", args.end-args.start)
         savepath = os.path.join(root, args.saveformat % i)
         save = load_save(savepath)
-        params, density_grid = load_params(save)
+        params, _ = load_params(save)
         if args.initexportformat:
             os.makedirs(os.path.dirname(args.initexportformat), exist_ok=True)
             params_size, params_csr_size = get_size(params)
-            density_grid_size, density_grid_csr_size = get_size(density_grid)
-            print_data(f"{args.T}  init", params_size, params_csr_size, density_grid_size, density_grid_csr_size)
+            print_data(f"{args.T}  init", params_size, params_csr_size)
             export_data(dict(
                 params_size=params_size,
                 params_csr_size=params_csr_size,
-                density_grid_size=density_grid_size,
-                density_grid_csr_size=density_grid_csr_size,
             ), args.initexportformat % i)
 
         if args.intraexportformat:
             os.makedirs(os.path.dirname(args.intraexportformat), exist_ok=True)
-            intra_params = compute_intra_density_grid(params, last_diff_params, args.T)
-            intra_density_grid = compute_intra_params(density_grid, last_diff_density_grid, args.T)
+            intra_params = compute_intra_params(params, last_diff_params, args.T)
             intra_params_size, intra_params_csr_size = get_size(intra_params)
-            intra_density_grid_size, intra_density_grid_csr_size = get_size(intra_density_grid)
-            print_data(f"{args.T} intra:", intra_params_size, intra_params_csr_size, intra_density_grid_size, intra_density_grid_csr_size)
+            print_data(f"{args.T} intra:", intra_params_size, intra_params_csr_size)
             export_data(dict(
                 intra_params_size=intra_params_size,
                 intra_params_csr_size=intra_params_csr_size,
-                intra_density_grid_size=intra_density_grid_size,
-                intra_density_grid_csr_size=intra_density_grid_csr_size,
             ), args.intraexportformat % {'i':i, "T": args.T})
 
         if args.interexportformat:
             os.makedirs(os.path.dirname(args.interexportformat), exist_ok=True)
             diff_params = compute_diff_params(params, last_diff_params, args.T)
-            diff_density_grid = compute_diff_density_grid(density_grid, last_diff_density_grid, args.T)
             last_diff_params += diff_params
-            last_diff_density_grid += diff_density_grid
             
             diff_params_size, diff_params_csr_size = get_size(diff_params)
-            diff_density_grid_size, diff_density_grid_csr_size = get_size(diff_density_grid)
-            print_data(f"{args.T}  diff:", diff_params_size, diff_params_csr_size, diff_density_grid_size, diff_density_grid_csr_size)
+            print_data(f"{args.T}  diff:", diff_params_size, diff_params_csr_size)
             export_data(dict(
                 diff_params_size=diff_params_size,
                 diff_params_csr_size=diff_params_csr_size,
-                diff_density_grid_size=diff_density_grid_size,
-                diff_density_grid_csr_size=diff_density_grid_csr_size,
             ), args.interexportformat % {'i':i, "T": args.T})
