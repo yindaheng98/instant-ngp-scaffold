6fbc7abbf7d2b3b826568b9f6eda101dc07645dc
yindaheng98 <yindaheng98@gmail.com>
2024-04-05 21:56:45 -0700

    do in all levels

--- run_count_size.sh
+++ run_count_size.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-command() {
+command0() {
     echo $1 start
     python ./scripts/parse_seq_size.py \
         --start $2 --end $3 \
         --saveformat results/$4/frame%d.bson \
         --fullexportformat results/$4/size/frame%d.json \
+        -T $1
+    echo $1 complete
+}
+
+command1() {
+    echo $1 start
+    python ./scripts/parse_seq_size.py \
+        --start $2 --end $3 \
+        --saveformat results/$4/frame%d.bson \
         --interexportformat results/$4/size/frame%\(i\)dT=%\(T\)fL=%\(L\)s-inter.json \
         -T 0.01 -L $1
     echo $1 complete
@@ -13,10 +22,12 @@ command() {
 doall() {
     ARGS="$1"
     echo Start Group:
-    command 0.2 $ARGS &
-    command 0.4 $ARGS &
-    command 0.6 $ARGS &
-    command 0.8 $ARGS &
+    command0 0.0 $ARGS &
+    command1 0.2 $ARGS &
+    command1 0.4 $ARGS &
+    command1 0.6 $ARGS &
+    command1 0.8 $ARGS &
+    command1 1.0 $ARGS &
     wait
 }
 
