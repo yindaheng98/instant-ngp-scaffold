e429cd6c6a90f5799d3dd3083d87cc75ec4b989a
yindaheng98 <yindaheng98@gmail.com>
2024-04-08 21:45:14 -0700

    nice

--- run_network_test.sh
+++ run_network_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-command() {
+command0() {
     ./build/instant-ngp-replay \
         --start $1 --end $2 \
         --load_snapshot results/$3/frame1.bson \
@@ -8,7 +8,17 @@ command() {
         --savecam $4 \
         --gethit results/$3/gridhit/M=$5/$6/frame%d.bson \
         --M_blimit $5 \
-        --save_image results/$3/gridhit/M=$5/img/%d.bin
+        --save_image results/$3/gridhit/M=$5/$6/img/gt/%d.bin
 }
-
-command 2 100 stnerf-taekwondo-regularization-1e-7 camera/stnerf-taekwondo-1.txt 200000 cam2
+command1() {
+    ./build/instant-ngp-replay \
+        --start $1 --end $2 \
+        --load_snapshot results/$3/frame1.bson \
+        --init results/$3/intra/frame1.bson \
+        --frameformat results/$3/gridhit/M=$5/$6/frame%d.bson \
+        --savecam $4 --directcam \
+        --M_blimit $5 \
+        --save_image results/$3/gridhit/M=$5/$6/img/lr/%d.bin
+}
+command0 2 100 stnerf-taekwondo-regularization-1e-7 camera/stnerf-taekwondo-1.txt 200000 cam1
+command1 2 100 stnerf-taekwondo-regularization-1e-7 camera/stnerf-taekwondo-1.txt 200000 cam1
