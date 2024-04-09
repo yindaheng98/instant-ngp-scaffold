552a9f3097760eb357bb333f9f9fd925cf70a1b1
yindaheng98 <yindaheng98@gmail.com>
2024-04-08 22:16:14 -0700

    害挺准

--- scripts/eval_cameras.py
+++ scripts/eval_cameras.py
@@ -4,6 +4,7 @@ import bson
 import numpy as np
 import zlib
 from scipy import sparse
+from common import linear_to_srgb
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--start", type=int, required=True, help="The start frame number.")
@@ -25,12 +26,15 @@ def calculate_psnr(img1, img2, max_value=255):
 
 if __name__ == "__main__":
     import os
+    import matplotlib.pyplot as plt
     args = parser.parse_args()
     root = os.getcwd()
     for i in range(args.start, args.end + 1):
         gtpath, lrpath = args.gtformat % i, args.lrformat % i
         gt = np.fromfile(gtpath, dtype='float32').reshape((1080, 1920, 4))
         lr = np.fromfile(lrpath, dtype='float32').reshape((1080, 1920, 4))
+        gt = np.clip(linear_to_srgb(gt[...,:3]), 0.0, 1.0)
+        lr = np.clip(linear_to_srgb(lr[...,:3]), 0.0, 1.0)
         modelpath = args.modelformat % i
         with open(modelpath, "rb") as f:
             model = bson.decode_all(f.read())[0]
@@ -39,4 +43,10 @@ if __name__ == "__main__":
         inter_zlib = get_size(inter)
         psnr = calculate_psnr(lr, gt, max_value=1)
         print(intra_zlib, inter_zlib, psnr)
+        # fig = plt.figure(figsize=(6, 3))
+        # ax = fig.subplots(nrows=1, ncols=2)
+        # ax[0].imshow(gt)
+        # ax[1].imshow(lr)
+        # plt.show()
+        # plt.close(fig)
         pass
