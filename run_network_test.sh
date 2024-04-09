c72e1d9c35a9ade0171700fc26414e73222fa94a
yindaheng98 <yindaheng98@gmail.com>
2024-04-08 22:30:01 -0700

    dump data

--- run_network_test.sh
+++ run_network_test.sh
@@ -20,5 +20,14 @@ command1() {
         --M_blimit $5 \
         --save_image results/$3/gridhit/M=$5/$6/img/lr/%d.bin
 }
+command2() {
+    python scripts/eval_cameras.py \
+        --start $1 --end $2 \
+        --modelformat results/$3/gridhit/M=$5/$6/frame%d.bson \
+        --gtformat results/$3/gridhit/M=$5/$6/img/gt/%d.bin \
+        --lrformat results/$3/gridhit/M=$5/$6/img/lr/%d.bin \
+        --save results/$3/gridhit/M=$5/$6"_result.json"
+}
 command0 2 100 stnerf-taekwondo-regularization-1e-7 camera/stnerf-taekwondo-1.txt 200000 cam1
 command1 2 100 stnerf-taekwondo-regularization-1e-7 camera/stnerf-taekwondo-1.txt 200000 cam1
+command2 2 100 stnerf-taekwondo-regularization-1e-7 camera/stnerf-taekwondo-1.txt 200000 cam1
