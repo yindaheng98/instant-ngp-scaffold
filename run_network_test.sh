c9eced31dfc1f6d1a1c83e83934e478ed26d5981
yindaheng98 <yindaheng98@gmail.com>
2024-04-08 21:30:14 -0700

    test the network

---
+++ run_network_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+command() {
+    ./build/instant-ngp-replay \
+        --start $1 --end $2 \
+        --load_snapshot results/$3/frame1.bson \
+        --init results/$3/intra/frame1.bson \
+        --frameformat results/$3/intra/frame%d.bson \
+        --savecam $4 \
+        --gethit results/$3/gridhit/M=$5/$6/frame%d.bson \
+        --M_blimit $5 \
+        --save_image results/$3/gridhit/M=$5/img/%d.bin
+}
+
+command 2 100 stnerf-taekwondo-regularization-1e-7 camera/stnerf-taekwondo-1.txt 200000 cam2
