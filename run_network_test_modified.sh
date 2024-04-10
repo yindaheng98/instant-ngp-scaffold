e37810afde0017a13f015ad3319b4ecf89c786e7
yindaheng98 <yindaheng98@gmail.com>
2024-04-09 22:18:07 -0700

    modified

--- run_network_test_modified.sh
+++ run_network_test_modified.sh
@@ -6,27 +6,27 @@ command0() {
         --init results/$3/intra/frame1.bson \
         --frameformat results/$3/intra/frame%d.bson \
         --savecam $4 \
-        --gethit results/$3/gridhit/M=$5/$6/frame%d.bson \
+        --gethit results/$3/gridhit/M=$5/$6/frame%d-modified.bson \
         --M_blimit $5 \
-        --save_image results/$3/gridhit/M=$5/$6/img/gt/%d.bin
+        --save_image results/$3/gridhit/M=$5/$6/img/gt/%d-modified.bin
 }
 command1() {
     ./build/instant-ngp-replay \
         --start $1 --end $2 \
         --load_snapshot results/$3/frame1.bson \
         --init results/$3/intra/frame1.bson \
-        --frameformat results/$3/gridhit/M=$5/$6/frame%d.bson \
+        --frameformat results/$3/gridhit/M=$5/$6/frame%d-modified.bson \
         --savecam $4 --directcam \
         --M_blimit $5 \
-        --save_image results/$3/gridhit/M=$5/$6/img/lr/%d.bin
+        --save_image results/$3/gridhit/M=$5/$6/img/lr/%d-modified.bin
 }
 command2() {
     python scripts/eval_cameras.py \
         --start $1 --end $2 \
-        --modelformat results/$3/gridhit/M=$5/$6/frame%d.bson \
-        --gtformat results/$3/gridhit/M=$5/$6/img/gt/%d.bin \
-        --lrformat results/$3/gridhit/M=$5/$6/img/lr/%d.bin \
-        --save results/$3/gridhit/M=$5/$6"_result.json"
+        --modelformat results/$3/gridhit/M=$5/$6/frame%d-modified.bson \
+        --gtformat results/$3/gridhit/M=$5/$6/img/gt/%d-modified.bin \
+        --lrformat results/$3/gridhit/M=$5/$6/img/lr/%d-modified.bin \
+        --save results/$3/gridhit/M=$5/$6"-modified_result.json"
 }
 command0 2 100 sear_steak-regularization-1e-7 camera/sear_steak.txt 200000 cam
 command1 2 100 sear_steak-regularization-1e-7 camera/sear_steak.txt 200000 cam
