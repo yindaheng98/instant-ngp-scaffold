35838334993eed03dc56ab98d4890f81809bd0d7
yindaheng98 <yindaheng98@gmail.com>
2024-05-05 22:39:22 -0700

    do with camera

--- run_train_grayscale.sh
+++ run_train_grayscale.sh
@@ -1,20 +1,29 @@
+mkdir -p results/grayscale
 train_color() {
     ./build/instant-ngp-train \
         --step=$3 \
-        --save_snapshot results/grayscale/$2-color-frame0-$4.bson \
+        --save_snapshot results/grayscale/$2-color-frame1-$4.bson \
         -c configs/nerf/$4.json \
         data/nerf/$1/frame1
 }
-mkdir -p results/grayscale
+render_color() {
+    mkdir -p results/grayscale/$2-color-frame1-$4
+    ./build/instant-ngp-viewer \
+        --load_snapshot results/grayscale/$2-color-frame1-$4.bson \
+        --savecam $5 \
+        --save_image results/grayscale/$2-color-frame1-$4/%d.bin \
+        data/nerf/$1/frame1
+}
 train_grayscale() {
     ./build/instant-ngp-train \
         --step=$3 \
-        --save_snapshot results/grayscale/$2-grayscale-frame0-$4.bson \
+        --save_snapshot results/grayscale/$2-grayscale-frame1-$4.bson \
         -c configs/nerf/$4.json \
         data/nerf/$1/grayscale/frame1
 }
 
 train_color taekwondo stnerf-taekwondo 30000 base
+render_color taekwondo stnerf-taekwondo '' base camera/stnerf-taekwondo-1.txt
 train_color taekwondo stnerf-taekwondo 30000 base_14
 train_color taekwondo stnerf-taekwondo 30000 base_15
 train_color taekwondo stnerf-taekwondo 30000 base_16
