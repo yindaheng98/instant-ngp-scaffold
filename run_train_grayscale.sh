28581ffe7a8e0aaea5ba03689ab3e10dfc7d75fa
yindaheng98 <yindaheng98@gmail.com>
2024-05-05 22:04:38 -0700

    train gray and color

--- run_train_grayscale.sh
+++ run_train_grayscale.sh
@@ -1,9 +1,26 @@
-command0() {
-    mkdir -p results/grayscale
+train_color() {
     ./build/instant-ngp-train \
         --step=$3 \
-        --save_snapshot results/grayscale/$2-frame0.bson \
+        --save_snapshot results/grayscale/$2-color-frame0-$4.bson \
+        -c configs/nerf/$4.json \
+        data/nerf/$1/frame1
+}
+mkdir -p results/grayscale
+train_grayscale() {
+    ./build/instant-ngp-train \
+        --step=$3 \
+        --save_snapshot results/grayscale/$2-grayscale-frame0-$4.bson \
+        -c configs/nerf/$4.json \
         data/nerf/$1/grayscale/frame1
 }
 
-command0 taekwondo stnerf-taekwondo 30000 1
+train_color taekwondo stnerf-taekwondo 30000 base
+train_color taekwondo stnerf-taekwondo 30000 base_14
+train_color taekwondo stnerf-taekwondo 30000 base_15
+train_color taekwondo stnerf-taekwondo 30000 base_16
+train_color taekwondo stnerf-taekwondo 30000 base_17
+train_grayscale taekwondo stnerf-taekwondo 30000 base
+train_grayscale taekwondo stnerf-taekwondo 30000 base_14
+train_grayscale taekwondo stnerf-taekwondo 30000 base_15
+train_grayscale taekwondo stnerf-taekwondo 30000 base_16
+train_grayscale taekwondo stnerf-taekwondo 30000 base_17
