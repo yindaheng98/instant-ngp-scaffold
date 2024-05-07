1a90e9b334ce4fa06ac2594d180c5aa403ccc686
yindaheng98 <yindaheng98@gmail.com>
2024-05-07 00:15:04 -0700

    do it

--- run_train_grayscale.sh
+++ run_train_grayscale.sh
@@ -104,12 +104,15 @@ eval_both() {
     rm results/grayscale/$2-gray-frame1-$4/camera-$5/*.bin
 }
 # eval_both taekwondo stnerf-taekwondo '' base 1 # debug
+vq_both() {
+    vq_color $1 $2 $3
+    vq_gray $1 $2 $3
+}
+# vq_both stnerf-taekwondo base 8 # debug
 eval_vq() {
-    vq_color $2 $4 $6
     render_vq_color $1 $2 '' $4 $5 $6
     convert_vq_color '' $2 '' $4 $5 $6
     rm results/grayscale/$2-color-frame1-$4/kmeans-$6/camera-$5/*.bin
-    vq_gray $2 $4 $6
     render_vq_gray $1 $2 '' $4 $5 $6
     convert_vq_gray '' $2 '' $4 $5 $6
     rm results/grayscale/$2-gray-frame1-$4/kmeans-$6/camera-$5/*.bin
@@ -118,97 +121,71 @@ eval_vq() {
 
 train_all() {
     train_both $1 $2 $3 base
-    train_both $1 $2 $3 base_18
-    train_both $1 $2 $3 base_18_3
-    train_both $1 $2 $3 base_18_2
-    train_both $1 $2 $3 base_18_1
-    train_both $1 $2 $3 base_17
-    train_both $1 $2 $3 base_17_3
-    train_both $1 $2 $3 base_17_2
-    train_both $1 $2 $3 base_17_1
-    train_both $1 $2 $3 base_16
-    train_both $1 $2 $3 base_16_3
-    train_both $1 $2 $3 base_16_2
-    train_both $1 $2 $3 base_16_1
-    train_both $1 $2 $3 base_15
-    train_both $1 $2 $3 base_15_3
-    train_both $1 $2 $3 base_15_2
-    train_both $1 $2 $3 base_15_1
-    train_both $1 $2 $3 base_14
-    train_both $1 $2 $3 base_14_3
-    train_both $1 $2 $3 base_14_2
-    train_both $1 $2 $3 base_14_1
+    vq_both $2 base 10
+    vq_both $2 base 9
+    vq_both $2 base 8
+    vq_both $2 base 7
+    vq_both $2 base 6
 }
 eval_all() {
-    eval_both $1 $2 $3 base $4
-    eval_both $1 $2 $3 base_17 $4
-    eval_both $1 $2 $3 base_17_3 $4
-    eval_both $1 $2 $3 base_17_2 $4
-    eval_both $1 $2 $3 base_17_1 $4
-    eval_both $1 $2 $3 base_16 $4
-    eval_both $1 $2 $3 base_16_3 $4
-    eval_both $1 $2 $3 base_16_2 $4
-    eval_both $1 $2 $3 base_16_1 $4
-    eval_both $1 $2 $3 base_15 $4
-    eval_both $1 $2 $3 base_15_3 $4
-    eval_both $1 $2 $3 base_15_2 $4
-    eval_both $1 $2 $3 base_15_1 $4
-    eval_both $1 $2 $3 base_14 $4
-    eval_both $1 $2 $3 base_14_3 $4
-    eval_both $1 $2 $3 base_14_2 $4
-    eval_both $1 $2 $3 base_14_1 $4
+    eval_both $1 $2 '' base $3
+    eval_vq $1 $2 '' base $3 10
+    eval_vq $1 $2 '' base $3 9
+    eval_vq $1 $2 '' base $3 8
+    eval_vq $1 $2 '' base $3 7
+    eval_vq $1 $2 '' base $3 6
 }
 
 train_all taekwondo stnerf-taekwondo 30000
-eval_all taekwondo stnerf-taekwondo 30000 1
-eval_all taekwondo stnerf-taekwondo 30000 2
-eval_all taekwondo stnerf-taekwondo 30000 3
-eval_all taekwondo stnerf-taekwondo 30000 4
+eval_all taekwondo stnerf-taekwondo 1
+eval_all taekwondo stnerf-taekwondo 2
+eval_all taekwondo stnerf-taekwondo 3
+eval_all taekwondo stnerf-taekwondo 4
 
 train_all walking stnerf-walking 30000 1
-eval_all walking stnerf-walking 30000 1
-eval_all walking stnerf-walking 30000 2
-eval_all walking stnerf-walking 30000 3
-eval_all walking stnerf-walking 30000 4
+eval_all walking stnerf-walking 1
+eval_all walking stnerf-walking 2
+eval_all walking stnerf-walking 3
+eval_all walking stnerf-walking 4
 
 train_all coffee_martini coffee_martini 30000 1
-eval_all coffee_martini coffee_martini 30000 1
-eval_all coffee_martini coffee_martini 30000 2
-eval_all coffee_martini coffee_martini 30000 3
-eval_all coffee_martini coffee_martini 30000 4
+eval_all coffee_martini coffee_martini 1
+eval_all coffee_martini coffee_martini 2
+eval_all coffee_martini coffee_martini 3
+eval_all coffee_martini coffee_martini 4
 
 train_all flame_steak flame_steak 30000 1
-eval_all flame_steak flame_steak 30000 1
-eval_all flame_steak flame_steak 30000 2
-eval_all flame_steak flame_steak 30000 3
-eval_all flame_steak flame_steak 30000 4
+eval_all flame_steak flame_steak 1
+eval_all flame_steak flame_steak 2
+eval_all flame_steak flame_steak 3
+eval_all flame_steak flame_steak 4
 
 train_all sear_steak sear_steak 30000 1
-eval_all sear_steak sear_steak 30000 1
-eval_all sear_steak sear_steak 30000 2
-eval_all sear_steak sear_steak 30000 3
-eval_all sear_steak sear_steak 30000 4
+eval_all sear_steak sear_steak 1
+eval_all sear_steak sear_steak 2
+eval_all sear_steak sear_steak 3
+eval_all sear_steak sear_steak 4
 
 train_all discussion discussion 30000 1
-eval_all discussion discussion 30000 1
-eval_all discussion discussion 30000 2
-eval_all discussion discussion 30000 3
-eval_all discussion discussion 30000 4
+eval_all discussion discussion 1
+eval_all discussion discussion 2
+eval_all discussion discussion 3
+eval_all discussion discussion 4
 
 train_all stepin stepin 30000 1
-eval_all stepin stepin 30000 1
-eval_all stepin stepin 30000 2
-eval_all stepin stepin 30000 3
-eval_all stepin stepin 30000 4
+eval_all stepin stepin 1
+eval_all stepin stepin 2
+eval_all stepin stepin 3
+eval_all stepin stepin 4
 
 train_all trimming trimming 30000 1
-eval_all trimming trimming 30000 1
-eval_all trimming trimming 30000 2
-eval_all trimming trimming 30000 3
-eval_all trimming trimming 30000 4
+eval_all trimming trimming 1
+eval_all trimming trimming 2
+eval_all trimming trimming 3
+eval_all trimming trimming 4
 
 train_all vrheadset vrheadset 30000 1
-eval_all vrheadset vrheadset 30000 1
-eval_all vrheadset vrheadset 30000 2
-eval_all vrheadset vrheadset 30000 3
-eval_all vrheadset vrheadset 30000 4
+eval_all vrheadset vrheadset 1
+eval_all vrheadset vrheadset 2
+eval_all vrheadset vrheadset 3
+eval_all vrheadset vrheadset 4
