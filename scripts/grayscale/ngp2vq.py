f04178b356eedabdb28ae5830fef4be5fa917f0e
yindaheng98 <yindaheng98@gmail.com>
2024-05-06 18:53:18 -0700

    ready for VQ

---
+++ scripts/grayscale/ngp2vq.py
@@ -0,0 +1,47 @@
+import argparse
+import bson
+import numpy as np
+import zlib
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--src", type=str, required=True, help="The source bson.")
+parser.add_argument("--dst", type=str, required=True, help="The destination bson.")
+parser.add_argument("--from-layer", type=int, default=0, help="Qualtize from which layer.")
+
+OFFSET0 = 10240
+N_FEATURES_PER_LEVEL = 4
+OFFSET_TABLE = [0, 4096, 89280, 613568, 1137856, 1662144, 2186432, 2710720, 3235008]
+
+
+def load_save(path):
+    with open(path, "rb") as f:
+        return bson.decode_all(f.read())[0]
+
+
+def load_params(save):
+    snapshot = save['snapshot']
+    params_bin = snapshot['params_binary']
+    params_type = snapshot['params_type']
+    if params_type == "__half":
+        params = np.frombuffer(params_bin, dtype=np.float16)
+    else:
+        params = np.frombuffer(params_bin, dtype=np.float32).astype(np.float16)
+    return params
+
+
+def dump_save(path, save, params):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    params_bin = params.tobytes()
+    save['snapshot']['params_binary'] = params_bin
+    with open(path, "wb") as f:
+        f.write(bson.encode(save))
+
+
+if __name__ == "__main__":
+    import os
+    args = parser.parse_args()
+    save = load_save(args.src)
+    params = load_params(save)
+    params_vector = params[OFFSET0:].reshape(-1, N_FEATURES_PER_LEVEL)
+    print(save)
