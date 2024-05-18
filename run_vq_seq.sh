e38de9ab41263a467cc501d47a26a2eead51b449
yindaheng98 <yindaheng98@gmail.com>
2024-05-17 23:15:35 -0700

    just not work

--- run_vq_seq.sh
+++ run_vq_seq.sh
@@ -10,12 +10,12 @@ render_color() {
         --savecam camera/$4.txt \
         --save_image results/$3/color/kmeans-none/$4/%d.bin
 }
-# render_color 2 100 coffee_martini-regularization-1e-7 coffee_martini-1 # debug
+# render_color 2 100 coffee_martini-regularization-none coffee_martini-1 # debug
 convert_color() {
     python scripts/grayscale/bin2image.py \
         --format results/$1/color/kmeans-none/$2/%d.bin
 }
-# convert_color coffee_martini-regularization-1e-7 coffee_martini-1 # debug
+# convert_color coffee_martini-regularization-none coffee_martini-1 # debug
 vq_color() {
     mkdir -p results/$3/color/kmeans-$4/models/intra
     python scripts/grayscale/ngp2vq_intra.py \
@@ -28,11 +28,11 @@ vq_color() {
         python scripts/grayscale/ngp2vq_intra.py \
             --src results/$3/intra/frame$i.bson \
             --dst results/$3/color/kmeans-$4/models/intra/frame$i.bson \
-            --save-kmeans results/$3/color/kmeans-$4/kmeans-params.pkl \
-            --log2-clusters $4
+            --log2-clusters $4 \
+            # --save-kmeans results/$3/color/kmeans-$4/kmeans-params.pkl
     done
 }
-# vq_color 2 100 coffee_martini-regularization-1e-7 6
+vq_color 2 100 coffee_martini-regularization-none 6
 render_vq_color() {
     mkdir -p results/$3/color/kmeans-$4
     ./build/instant-ngp-replay \
@@ -43,4 +43,4 @@ render_vq_color() {
         --savecam camera/$5.txt \
         --save_image results/$3/color/kmeans-$4/$5/%d.bin
 }
-render_vq_color 2 100 coffee_martini-regularization-1e-7 6 coffee_martini-1 # debug
+render_vq_color 2 100 coffee_martini-regularization-none 6 coffee_martini-1 # debug
