a2fadd2fcf952cda74ad66077af836e9c87c6198
yindaheng98 <yindaheng98@gmail.com>
2024-05-27 15:24:21 -0700

    do sth with depth

---
+++ debug/show_rgbd.py
@@ -0,0 +1,30 @@
+import open3d as o3d
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+print("Read Redwood dataset")
+redwood_rgbd = o3d.data.SampleRedwoodRGBDImages()
+color_raw = cv2.imread("results/coffee_martini-regularization-none-gray/color/kmeans-none/coffee_martini-1/33.png")
+depth_raw = np.load("results/coffee_martini-regularization-none-gray/color/kmeans-none/coffee_martini-1/33.npz")["rgbd"][..., -1]
+# color_raw = o3d.io.read_image(redwood_rgbd.color_paths[0])
+# depth_raw = o3d.io.read_image(redwood_rgbd.depth_paths[0])
+depth_raw = (depth_raw * 65536).astype(np.uint16)
+rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(o3d.geometry.Image(color_raw), o3d.geometry.Image(depth_raw))
+print(rgbd_image)
+
+plt.subplot(1, 2, 1)
+plt.title('Redwood grayscale image')
+plt.imshow(rgbd_image.color)
+plt.subplot(1, 2, 2)
+plt.title('Redwood depth image')
+plt.imshow(rgbd_image.depth)
+plt.show()
+
+pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
+    rgbd_image,
+    o3d.camera.PinholeCameraIntrinsic(
+        o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
+# Flip it, otherwise the pointcloud will be upside down
+pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+o3d.visualization.draw_geometries([pcd])
--- scripts/grayscale/bin2image.py
+++ scripts/grayscale/bin2image.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+import os
 import cv2
 
 parser = argparse.ArgumentParser()
@@ -19,10 +20,14 @@ if __name__ == "__main__":
     i = 0
     while os.path.isfile(args.format % i):
         binpath = args.format % i
-        img = np.fromfile(binpath, dtype='float32').reshape((1080, 1920, 4))
-        img = np.clip(linear_to_srgb(img[..., :3]), 0.0, 1.0)
+        raw = np.fromfile(binpath, dtype='float32').reshape((1080, 1920, 5))
+        rgba = np.fromfile(binpath, dtype='float32')[:1080*1920*4].reshape((1080, 1920, 4))
+        img = np.clip(linear_to_srgb(rgba[..., :3]), 0.0, 1.0)
         img_cv = cv2.cvtColor((img*255).astype(np.uint8), cv2.COLOR_BGR2RGB)
         imgpath = os.path.splitext(binpath)[0] + ".png"
         cv2.imwrite(imgpath, img_cv)
+        depth = np.fromfile(binpath, dtype='float32')[1080*1920*4:].reshape((1080, 1920, 1))
+        rgbd = np.concatenate([img, depth], axis=-1)
+        np.savez_compressed(os.path.splitext(binpath)[0] + '.npz', rgbd=rgbd)
         print(binpath, imgpath)
         i += 1
